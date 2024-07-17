64c308ae64897f9c4fb1e073f4cf6ed07a60119a
MARKRET-RUBA <markretruba234@gmail.com>
2024-07-17 22:37:13 +0530

    first commit

---
+++ hell.html/script.js
@@ -0,0 +1,60 @@
+//countdown to Independence day
+setTimeout(function(){
+    console.log("10");
+    //display on the screen
+    document.body.innerHTML="<h2>10</h2>";
+    setTimeout(function(){
+        console.log("9");
+        //display on the screen
+        document.body.innerHTML="<h2>9</h2>";
+        setTimeout(function(){
+            console.log("8");
+            //display on the screen
+            document.body.innerHTML="<h2>8</h2>";
+            setTimeout(function(){
+                console.log("7");
+                //display on the screen
+                document.body.innerHTML="<h2>7</h2>";
+                setTimeout(function(){
+                    console.log("6");
+                    //display on the screen
+                    document.body.innerHTML="<h2>6</h2>";
+                    setTimeout(function(){
+                        console.log("5");
+                        //display on the screen
+                        document.body.innerHTML="<h2>5</h2>";
+                        setTimeout(function(){
+                            console.log("4");
+                            //display on the screen
+                            document.body.innerHTML="<h2>4</h2>";
+                            setTimeout(function(){
+                                console.log("3");
+                                //display on the screen
+                                document.body.innerHTML="<h2>3</h2>";
+                                setTimeout(function(){
+                                    console.log("2");
+                                    //display on the screen
+                                    document.body.innerHTML="<h2>2</h2>";
+                                    setTimeout(function(){
+                                        console.log("1");
+                                        //display on the screen
+                                        document.body.innerHTML="<h2>1</h2>";
+                                        setTimeout(function(){
+                                            console.log("Happy Independence Day!");
+                                            //display on the screen
+                                            document.body.innerHTML="<h1>Happy Independence Day!</h1>";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+             },1000);
+        },1000);
+    },1000);
+},1000);
+                                   
+
+                                            
+   
